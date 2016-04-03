ed11d2c3faa63966406fb41e056de5f781f9bd2c
Kent deVillafranca <kent@kentdev.net>
2016-04-03 19:16:22 -0400

    Updated test program

--- test.cpp
+++ test.cpp
@@ -2,17 +2,56 @@
 #include <iostream>
 using namespace std;
 
+void listGroup(const UHDF_Group &g, int depth)
+{
+    for (const auto &name : g.getGroupNames())
+    {
+        for (int i = 0; i < depth; i++)
+            cout << "\t";
+
+        cout << "\tGROUP '" << name << "'" << endl;
+        const UHDF_Group g = g.openGroup(name);
+        listGroup (g, depth + 1);
+    }
+    for (const auto &name : g.getDatasetNames())
+    {
+        for (int i = 0; i < depth; i++)
+            cout << "\t";
+        cout << "\tFIELD '" << name << "'" << endl;
+    }
+}
+
 void listContents(const UHDF_File &f)
 {
     cout << f.getFileName() << ":" << endl;
     for (const auto &name : f.getGroupNames())
     {
-        cout << "\tGROUP " << name << endl;
+        cout << "\tGROUP '" << name << "'" << endl;
+        const UHDF_Group g = f.openGroup(name);
+        listGroup (g, 1);
     }
     for (const auto &name : f.getDatasetNames())
     {
-        cout << "\tFIELD " << name << endl;
+        cout << "\tFIELD '" << name << "'" << endl;
     }
+}
+
+void average(const UHDF_File &f, const string &field)
+{
+    const UHDF_Dataset d = f.openDataset(field);
+
+    const auto &dims = d.getDimensions();
+
+    const vector<double> data = d.readAll<double>();
+    double avg = 0;
+
+    for (auto d : data)
+    {
+        avg += d;
+    }
+    avg /= data.size();
+
+    cout << "Average value = " << avg << endl;
 }
 
 int main (int argc, char *argv[])
@@ -21,8 +60,11 @@ int main (int argc, char *argv[])
     UHDF_File testH5("hdf5_test.h5", UHDF_READONLY);
 
     listContents(testH4);
+
     listContents(testH5);
+
+    average(testH4, "Latitude");
+    average(testH5, "images/Iceberg");
 
     return 0;
 }
-
